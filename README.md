53892e952dd0fd6db6d0539ae483c6323e5eefd0
Anderson Lopes dos Santos <lopscorp@iMac-de-Anderson.local>
2020-10-24 07:17:33 -0300

    feature :sparkles: Start

---
+++ README.md
@@ -0,0 +1,2 @@
+## Referencia 
+https://github.com/juliocarneiro/react-chrome-extension/blob/master/src/App.js
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import './sytles/global.css'
+
+function App() {
+  return (
+    <Switch>
+      <Routes />
+    </Switch>
+  )
+}
+
+export default App
